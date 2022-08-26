5c0dab58f49a858df99adf78ee6a72867c696a82
Victoria Garcia <110563390+GVictoria90@users.noreply.github.com>
2022-08-25 21:05:54 -0300

    Add files via upload

---
+++ multiplos/src/index.ts
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+let rotulo = document.getElementById("rotulo");
+let btnEnv = document.getElementById("btnEnv");
+rotulo.innerHTML =
+  "Números que son múltiplos de 2 o de 3 que hay entre 1 y 100: ";
+btnEnv.addEventListener("click", () => {
+  let cadena: string = "";
+  for (let numero = 1; numero <= 100; numero++) {
+    if (numero % 2 === 0 || numero % 3 === 0) {
+      cadena = cadena + numero + "\n";
+    }
+  }
+  console.log(cadena);
+});
